4a549da018e6251e451d3a776f710882fe9b1393
Evgeniya Barteneva <evgeniyabarteneva1508@mail.ru>
2023-05-21 23:48:27 +0500

    Task 004 evenNList

---
+++ Task004_evenNList/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int endList = Convert.ToInt32(Console.ReadLine());
+int number = 1;
+while (number <= endList) //Введенное пользователем число включаем в перечень!
+{
+    if (number % 2 == 0)
+    {
+        Console.WriteLine(number);
+    }
+    number = number + 1;  
+}
+
