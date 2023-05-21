2b37f18ca6cdbe17bc87cef0aa5993b2cc3e716b
Evgeniya Barteneva <evgeniyabarteneva1508@mail.ru>
2023-05-21 22:41:15 +0500

    Task 003 evenNum

---
+++ Task003_evenNum/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("Введенное Вами число " + number + " является ЧЕТНЫМ");
+}
+else
+{
+    Console.WriteLine("Введенное Вами число " + number + " является НЕЧЕТНЫМ");
+}
