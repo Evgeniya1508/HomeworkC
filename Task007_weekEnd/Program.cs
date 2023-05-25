bd34213014df2a511b8c84c69199f30b9a252c2a
Evgeniya Barteneva <evgeniyabarteneva1508@mail.ru>
2023-05-26 00:48:52 +0500

    Task 007 weekEnd

---
+++ Task007_weekEnd/Program.cs
@@ -0,0 +1,29 @@
+﻿void daysOfWeek(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(1,8);
+    }
+}
+
+int[] arr = new int[7];
+daysOfWeek(arr);
+for (int i = 0; i < arr.Length; i++)
+{
+    int date = arr[i];
+    if (date < 6)
+    {
+        Console.WriteLine(arr [i] + " Соберись! Сегодня рабочий день!");
+    }
+    else if (date == 6)
+    {
+        Console.WriteLine(arr [i] + " Ура! Сегодня Выходной!");
+    }
+    else
+    {
+        Console.WriteLine(arr[i] + " Ура! Сегодня снова выходной!");
+    }
+}
+
+
