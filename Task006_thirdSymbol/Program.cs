4b3796c59312b31c99079ea83f1d7053c84f127b
Evgeniya Barteneva <evgeniyabarteneva1508@mail.ru>
2023-05-26 02:31:48 +0500

    Task006_thirdSymbol

---
+++ Task006_thirdSymbol/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите любое число до 9999");
+int number = int.Parse(Console.ReadLine());
+if (number < 100)
+{
+    Console.WriteLine("Третья цифра в введенном Вами числе ОТСУТСТВУЕТ");
+}
+else if (number < 1000)
+{
+    int thirdSymb = number % 10;
+    Console.WriteLine("Третья цифра в введенном Вами числе - " + thirdSymb);
+}
+else
+{
+    int thirdSymb = number / 10 % 10;
+    Console.WriteLine("Третья цифра в введенном Вами числе - " + thirdSymb);
+}
