5f492eb302eeb270f30aff323a18577554b9ed9c
Evgeniya Barteneva <evgeniyabarteneva1508@mail.ru>
2023-05-26 00:00:12 +0500

    Task005_secSymbol

---
+++ Task005_secSymbol/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите трехзначное число ");
+int number = int.Parse(Console.ReadLine());
+int secSymb = number / 10 % 10;
+Console.WriteLine("Вторая цифра в веденном Вами числе - " + secSymb);
