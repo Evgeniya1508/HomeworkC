f4335894f6638c7374da56541959a9ab9385aef7
Evgeniya Barteneva <evgeniyabarteneva1508@mail.ru>
2023-06-01 01:45:47 +0500

    Task 012 SumOfNum

---
+++ Task012_SumOfNum/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите любое целое положительное число");
+int number = int.Parse(Console.ReadLine());
+int sumOfNum = 0;
+int element = 0;
+if (number > 0)
+{
+    while (number > 0)
+    {
+    element = number % 10;
+    sumOfNum = sumOfNum + element;
+    number = number / 10;
+    }
+    Console.WriteLine($"Сумма цифр в заданном Вами числе составляет {sumOfNum}.");
+}
+else
+{
+    Console.WriteLine($"Ошибка! Ввведенное Вами число {number} является отрицательным! Введите только положительное число!!!");
+}
+
+    
+
