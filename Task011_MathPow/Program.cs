d7281f92fb2d4fe9bc650db63f068ad73e1c47d5
Evgeniya Barteneva <evgeniyabarteneva1508@mail.ru>
2023-06-01 00:58:38 +0500

    Task 011 MathPow

---
+++ Task011_MathPow/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите любое целое число (А)");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите любое целое число (B)");
+int numB = Convert.ToInt32(Console.ReadLine());
+double result = 1;
+for (int i = 1; i <= numB; i++)
+{
+    result = Math.Pow(numA,i);
+}
+Console.WriteLine($"Число А, возведенное в {numB} степень  равно {result}.");
+
