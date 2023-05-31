90a198c456743fce5686f75161563b5d89f30a73
Evgeniya Barteneva <evgeniyabarteneva1508@mail.ru>
2023-06-01 02:07:46 +0500

    Task013 arrayRand

---
+++ Task013_arrayRand/Program.cs
@@ -0,0 +1,12 @@
+﻿void randomNum(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(1,100);
+    }
+}
+int size = 8;
+int[] arr = new int[size];
+randomNum(arr);
+Console.WriteLine($"Maccив: [{String.Join("; ", arr)}]");
