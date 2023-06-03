869a8f531dd923246aadf6e1ab68815586ab47b1
Evgeniya Barteneva <evgeniyabarteneva1508@mail.ru>
2023-06-03 17:15:49 +0500

    Task014 count EvenNum

--- Task014_countEvenNum/Program.cs
+++ Task014_countEvenNum/Program.cs
@@ -22,10 +22,7 @@ int CountEvenNum(int[] array)
     int count = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i] % 2 == 0)
-        {
-            count+=1;
-        }
+        if (array[i] % 2 == 0) count += 1;
     }
     return count;
 }
